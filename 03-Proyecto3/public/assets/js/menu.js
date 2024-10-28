f9dabda36aed3a7059133c76a5f57edce8ffd665
Agustin Schwank <agustinschwank@hotmail.es>
2024-10-28 15:52:41 -0300

    upgrade

---
+++ 03-Proyecto3/public/assets/js/menu.js
@@ -0,0 +1,67 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', (e)=>{
+    
+    const mobile__btn = document.querySelector('.navbar__movile-btn');
+    const menu__mobile = document.querySelector('.menu-mobile');
+    const btn_close = document.querySelector('.menu-mobile__close');
+    
+
+    const show__hidden = () => {
+        let show = document.querySelector('.menu-mobile--show');
+        if (show) {
+            menu__mobile.classList.remove('menu-mobile--show');
+        } else {
+            menu__mobile.classList.add('menu-mobile--show');
+        }
+    }
+
+    mobile__btn.addEventListener('click', ()=>{show__hidden()});
+    btn_close.addEventListener('click', ()=> {show__hidden()});
+
+    window.addEventListener('resize', () => {
+        let width = parseInt(document.body.clientWidth);
+        if(width >= 1000){
+            menu__mobile.classList.remove('menu-mobile--show');
+        }
+    });
+
+    /* Submenu */
+
+    const menu_items = document.querySelectorAll('.menu-mobile__item');
+
+    menu_items.forEach((menu_item)=>{
+        menu_item.addEventListener('click', ()=>{
+            let aElement = menu_item.firstElementChild;
+            let iElement = aElement.lastChild;
+            let submenu = menu_item.lastElementChild;
+            let preview_submenu = menu_item.previousElementSibling;
+
+            if (submenu.className === 'menu-mobile__submenu-mobile') {
+                if(submenu.style.display === 'block') {
+                    submenu.style.display = 'none';
+                } else {
+                    submenu.style.display = 'block';
+                }
+                if (preview_submenu === 'block') {
+                    preview_submenu.style.display = 'none';
+                } 
+                
+            };
+    
+            if(iElement.classList){
+                if (!iElement.classList.contains('nav__icon--mobile-move')) {
+                    iElement.classList.add('nav__icon--mobile-move');
+                    iElement.classList.remove('nav__icon--move');
+                } else {
+                    iElement.classList.add('nav__icon--move');
+                    iElement.classList.remove('nav__icon--mobile-move');
+                }
+            }
+        });
+    });
+
+
+
+
+});
